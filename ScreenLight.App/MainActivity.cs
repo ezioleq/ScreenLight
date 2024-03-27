a1c9dffb38a8c262c652661d4680dd3eb6651d43
ezioleq <git@ezioleq.com>
2024-03-27 18:23:16 +0100

    WIP Toggling flashlight

--- ScreenLight.App/MainActivity.cs
+++ ScreenLight.App/MainActivity.cs
@@ -1,5 +1,6 @@
 using _Microsoft.Android.Resource.Designer;
 using Android.Content.PM;
+using Android.Graphics;
 using Android.Views;
 
 namespace ScreenLight.App;
@@ -15,10 +16,21 @@ public class MainActivity : Activity
     {
         base.OnCreate(savedInstanceState);
 
+        SetContentView(ResourceConstant.Layout.activity_main);
+
         HideNavigation();
         SetScreenBrightness(1f);
+        Window?.DecorView.SetBackgroundColor(Color.White);
 
-        SetContentView(ResourceConstant.Layout.activity_main);
+        var toggleButton = FindViewById<ImageView>(ResourceConstant.Id.toggleButton);
+        var clicked = false;
+
+        toggleButton!.Click += (_, _) =>
+        {
+            clicked = !clicked;
+
+            Window?.DecorView.SetBackgroundColor(clicked ? Color.White : Color.Black);
+        };
     }
 
     private void SetScreenBrightness(float brightness)
